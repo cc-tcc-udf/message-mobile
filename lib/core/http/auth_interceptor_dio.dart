cfa89a0533adbcf29441b2ef82a3b33279297451
Johnatan Caetano dos Santos <104772832+Johnatan-Caetano@users.noreply.github.com>
2024-06-17 18:13:51 -0300

    init: criação da estrutura do projeto

---
+++ lib/core/http/auth_interceptor_dio.dart
@@ -0,0 +1,31 @@
+import 'package:campus_connect/core/localstorage/security_shared_preference.dart';
+import 'package:dio/dio.dart';
+
+class AuthInterceptorDio extends Interceptor {
+  @override
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    final SecuritySharedPreference storage = SecuritySharedPreference();
+
+    if (options.headers['Authorization'] == null) {
+      var token = await storage.read("token");
+      if (token != null) {
+        options.headers = {"Authorization": 'Bearer $token'};
+      }
+    }
+    super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    // ignore: avoid_print
+    print(
+        'RESPONSE[${response.statusCode}] => PATH: ${response.requestOptions.path}');
+    super.onResponse(response, handler);
+  }
+
+  @override
+  Future onError(DioException err, ErrorInterceptorHandler handler) async {
+    super.onError(err, handler);
+  }
+}
---
+++ lib/core/injection/app_injection.dart
@@ -0,0 +1,14 @@
+import 'package:campus_connect/core/localstorage/security_shared_preference.dart';
+import 'package:get_it/get_it.dart';
+import '../http/http_client_dio.dart';
+import '../http/http_client_impl.dart';
+
+class AppInjection {
+  static final GetIt getIt = GetIt.instance;
+
+  static Future<void> init() async {
+    getIt
+      ..registerFactory<HttpClientDio>(() => HttpClientImpl())
+      ..registerLazySingleton(() => SecuritySharedPreference());
+  }
+}
